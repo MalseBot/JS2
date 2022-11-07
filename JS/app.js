6eca14454171a29e5d2c2195cc47470461a2040e
MalseBot <eso20016@gmail.com>
2022-11-07 16:19:42 +0200

    adding the API

--- JS/app.js
+++ JS/app.js
@@ -37,16 +37,16 @@ function clear()
 
 function displaymovies(arrayOfObject)
 {
-    var cartoona="";
+    var storage="";
     for (let index = 0; index < arrayOfObject.length; index++) {
-           cartoona += `<tr>
+           storage += `<tr>
            <td>${index}</td>
            <td>${arrayOfObject[index].movieName}</td>
            <td>${arrayOfObject[index].movieDesc}</td>
            <td><button type="button" onclick="deletemovie(${index});" class="btn btn-danger">Delete</button></td>`;
     }
 
-    document.getElementById("result").innerHTML = cartoona;
+    document.getElementById("result").innerHTML = storage;
 }
 
 window.onscroll = function () {
@@ -71,4 +71,59 @@ function welcome() {
     document.getElementById("VNdisplay").innerHTML =visiter.value;
     alert("Hope you have fun in our site")
 }
+
+const API_KEY = 'api_key=a6be3551036c2d575ac8c9c07fc2d683'
+const BASE_URL = 'https://api.themoviedb.org/3/'
+const API_URL = BASE_URL + 'discover/movie?' + API_KEY + '&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate'
+const IMG_URL = 'https://image.tmdb.org/t/p/w500'
+const main = document.getElementById('mostPopular');
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+const searchURL = BASE_URL + '/search/movie?' +API_KEY;
+
+getMovies(API_URL);
+
+
+function getMovies(url) {
+    fetch(url).then(res => res.json()).then(data => {
+        console.log(data.results);
+        showMovies(data.results);
+    })
+}
+
+function showMovies(data) {
+    main.innerHTML='';
+
+
+
+    data.forEach(movie => {
+
+        const {title, poster_path, overview} = movie;
+        const movieEl =document.createElement('div');
+    movieEl.classList.add('movie');
+movieEl.innerHTML = `<div class="col">
+<div class="card">
+  <img src="${IMG_URL +poster_path}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${title}</h5>
+    <p class="card-text">${overview}</p>
+  </div>
+</div>
+</div>`
+main.appendChild(movieEl);
+}
+)}
+              
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const searchTerm = search.value;
+
+    if (searchTerm) {
+        getMovies(searchURL+'&query='+searchTerm)
+    }else{
+        getMovies(API_URL);
+    }
+})
+        
 
